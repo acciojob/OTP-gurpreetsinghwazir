dadb8412a65cfa96a510c1212e5c619a73d00cc8
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-15 00:07:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let inPut = document.querySelectorAll(".code");
 for (let i = 0;  i < inPut.length -1; i++) {
 	inPut[i].addEventListener("keyup", Display)
-	
-	function Display(eventDetails) {
-		this.nextElementSibling.focus();
-		
-	}
-	
+		function Display(eventDetails) {
+			this.nextElementSibling.focus();	
+	}	
 }
+
+	
