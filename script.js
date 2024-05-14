71ebb4407661175d3d1ff3f22ddd2fd184bad1c2
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-14 14:13:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-$("input").keyup(function () {
-    if (this.value.length == this.maxLength) {
-      $(this).next('input').focus();
+const inputs = document.querySelectorAll("#code");
+for (let i = 0; i < inputs.length; i++) {
+  const input = inputs[i];
+
+  input.addEventListener("input", function () {
+    // handling normal input
+    if (input.value.length == 1 && i+1 < inputs.length) {
+     inputs[i+1].focus();
     }
-});
+    /* ... other codes (a) ... */
+  });
+  /* ... other codes (b) ... */
+}
