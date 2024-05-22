92ecf5eb5f9c8c03c243b8603462be6789f4d2f5
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-22 16:24:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let inPut = document.getElementsByClassName("code")
 
-let firstFoc = document.getElementsByClassName("code")[0];
-firstFoc.focus();  
- 
-for (let i = 0; i < inPut.length; i++) { 
+// let firstFoc = document.getElementsByClassName("code")[0];
+// firstFoc.focus();   
+   
+for (let i = 0; i < inPut.length; i++) {  
     inPut[i].addEventListener("keyup", Display)
-
+ 
     function Display(eventDetails) {
+
         if (eventDetails.keyCode == 8) { // Check if backspace key is pressed
             if (this.value === "" && i > 0) { // Check if current input is empty and not the first one
                 this.previousElementSibling.focus(); // Focus on the previous input
